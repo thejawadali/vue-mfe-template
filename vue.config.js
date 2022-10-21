941943fe1cdf9dd8a91a41ae54297a55bbd40fe5
Jawad Ali <jawad.ali@repairdesk.co>
2022-10-21 17:19:04 +0500

    set mfe

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,54 @@
 const { defineConfig } = require('@vue/cli-service')
+const { ModuleFederationPlugin } = require('webpack').container;
+const deps = require("./package.json").dependencies;
+
 module.exports = defineConfig({
-  filenameHashing: false,
-  transpileDependencies: true
+  pages: {
+    index: {
+      entry: './src/index.js',
+    },
+  },
+  publicPath: "auto",
+  transpileDependencies: true,
+  configureWebpack: {
+    optimization: {
+      splitChunks: false,
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        filename: 'remoteEntry.js',
+        exposes: {
+          // "./NavBar.vue": "./src/components/RealApp.vue"
+        },
+        remotes: {
+          // remote_1: "remote_1@http://localhost:3000/remoteEntry.js",
+        },
+        shared: {
+          ...deps,
+          vue: {
+            singleton: true,
+          },
+        },
+      }),
+    ],
+    
+  },
 })
+
+// defineConfig({
+//   filenameHashing: false,
+//   transpileDependencies: true,
+//   configureWebpack: {
+//     plugins: [
+//       new ModuleFederationPlugin({
+//         shared: {
+//           ...deps,
+//           vue: {
+//             singleton: true,
+//           },
+//         },
+//         remotes: {}
+//       })
+//     ]
+//   }
+// })
